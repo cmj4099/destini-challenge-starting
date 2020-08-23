71ec46ec161319913b41c9f880aecd49bf065dbd
User <moonchang04@naver.com>
2020-08-23 19:45:36 +0900

    이야잇

--- lib/story.dart
+++ lib/story.dart
@@ -1,5 +1,18 @@
-//TODO: Step 2 - Create a new class called Story.
+// TODO : 2 단계-Story라는 새 클래스를 만듭니다.
 
-//TODO: Step 3 - Create 3 properties for this class, A. storyTitle to store the story text. B. choice1 to store the text for choice 1, C. choice2 to store the text for choice 2.
 
-//TODO: Step 4 - Create a Constructor for this class to be able to initialise the properties created in step 3.
+class Story {
+
+ String storyTitle;
+
+ String choice1;
+
+ String choice2;
+
+
+ Story({this.storyTitle, this.choice1, this.choice2});//this생성자 제대로 안품 다시복습
+}
+
+// TODO : 3 단계-이 클래스에 대한 3 개의 속성 A. storyTitle을 만들어 스토리 텍스트를 저장합니다. B. choice1은 선택 1에 대한 텍스트를 저장하고 C. choice2는 선택 2에 대한 텍스트를 저장합니다.
+
+// TODO : 4 단계-3 단계에서 만든 속성을 초기화 할 수 있도록이 클래스에 대한 생성자를 만듭니다.
--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -1,59 +1,86 @@
-//TODO: Step 6 - import the story.dart file into this file.
+import 'story.dart';
 
-//TODO: Step 5 - Create a new class called StoryBrain.
+// TODO : 6 단계-story.dart 파일을이 파일로 가져옵니다.
 
-//TODO: Step 7 - Uncomment the lines below to include storyData as a private property in StoryBrain. Hint: You might need to change something in story.dart to make this work.
+// TODO : 5 단계-StoryBrain이라는 새 클래스를 만듭니다.
 
-//List<Story> _storyData = [
-//  Story(
-//      storyTitle:
-//      'Your car has blown a tire on a winding road in the middle of nowhere with no cell phone reception. You decide to hitchhike. A rusty pickup truck rumbles to a stop next to you. A man with a wide brimmed hat with soulless eyes opens the passenger door for you and asks: "Need a ride, boy?".',
-//      choice1: 'I\'ll hop in. Thanks for the help!',
-//      choice2: 'Better ask him if he\'s a murderer first.'),
-//  Story(
-//      storyTitle: 'He nods slowly, unphased by the question.',
-//      choice1: 'At least he\'s honest. I\'ll climb in.',
-//      choice2: 'Wait, I know how to change a tire.'),
-//  Story(
-//      storyTitle:
-//      'As you begin to drive, the stranger starts talking about his relationship with his mother. He gets angrier and angrier by the minute. He asks you to open the glovebox. Inside you find a bloody knife, two severed fingers, and a cassette tape of Elton John. He reaches for the glove box.',
-//      choice1: 'I love Elton John! Hand him the cassette tape.',
-//      choice2: 'It\'s him or me! You take the knife and stab him.'),
-//  Story(
-//      storyTitle:
-//      'What? Such a cop out! Did you know traffic accidents are the second leading cause of accidental death for most adult age groups?',
-//      choice1: 'Restart',
-//      choice2: ''),
-//  Story(
-//      storyTitle:
-//      'As you smash through the guardrail and careen towards the jagged rocks below you reflect on the dubious wisdom of stabbing someone while they are driving a car you are in.',
-//      choice1: 'Restart',
-//      choice2: ''),
-//  Story(
-//      storyTitle:
-//      'You bond with the murderer while crooning verses of "Can you feel the love tonight". He drops you off at the next town. Before you go he asks you if you know any good places to dump bodies. You reply: "Try the pier".',
-//      choice1: 'Restart',
-//      choice2: '')
-//];
+class StoryBrain{
+  List<Story> _storyData = [
+    Story(
+        storyTitle:
+        'Your car has blown a tire on a winding road in the middle of nowhere with no cell phone reception. You decide to hitchhike. A rusty pickup truck rumbles to a stop next to you. A man with a wide brimmed hat with soulless eyes opens the passenger door for you and asks: "Need a ride, boy?".',
+        choice1: 'I\'ll hop in. Thanks for the help!',
+        choice2: 'Better ask him if he\'s a murderer first.'),
+    Story(
+        storyTitle: 'He nods slowly, unphased by the question.',
+        choice1: 'At least he\'s honest. I\'ll climb in.',
+        choice2: 'Wait, I know how to change a tire.'),
+    Story(
+        storyTitle:
+        'As you begin to drive, the stranger starts talking about his relationship with his mother. He gets angrier and angrier by the minute. He asks you to open the glovebox. Inside you find a bloody knife, two severed fingers, and a cassette tape of Elton John. He reaches for the glove box.',
+        choice1: 'I love Elton John! Hand him the cassette tape.',
+        choice2: 'It\'s him or me! You take the knife and stab him.'),
+    Story(
+        storyTitle:
+        'What? Such a cop out! Did you know traffic accidents are the second leading cause of accidental death for most adult age groups?',
+        choice1: 'Restart',
+        choice2: ''),
+    Story(
+        storyTitle:
+        'As you smash through the guardrail and careen towards the jagged rocks below you reflect on the dubious wisdom of stabbing someone while they are driving a car you are in.',
+        choice1: 'Restart',
+        choice2: ''),
+    Story(
+        storyTitle:
+        'You bond with the murderer while crooning verses of "Can you feel the love tonight". He drops you off at the next town. Before you go he asks you if you know any good places to dump bodies. You reply: "Try the pier".',
+        choice1: 'Restart',
+        choice2: '')
+  ];
 
-//TODO: Step 23 - Use the storyNumber property inside getStory(), getChoice1() and getChoice2() so that it gets the updated story and choices rather than always just the first (0th) one.
 
-//TODO: Step 8 - Create a method called getStory() that returns the first storyTitle from _storyData.
+  String getStory(){
+    return _storyData[_storyNumber].storyTitle;
+  }
 
-//TODO: Step 11 - Create a method called getChoice1() that returns the text for the first choice1 from _storyData.
 
-//TODO: Step 12 - Create a method called getChoice2() that returns the text for the first choice2 from _storyData.
+// TODO : 7 단계-StoryBrain에서 storyData를 개인 속성으로 포함하도록 아래 줄의 주석 처리를 제거합니다. 힌트 :이 작업을 수행하려면 story.dart에서 무언가를 변경해야 할 수도 있습니다.
 
-//TODO: Step 25 - Change the storyNumber property into a private property so that only story_brain.dart has access to it. You can do this by right clicking on the name (storyNumber) and selecting Refactor -> Rename to make the change across all the places where it's used.
 
-//TODO: Step 16 - Create a property called storyNumber which starts with a value of 0. This will be used to track which story the user is currently viewing.
+// TODO : 23 단계-getStory (), getChoice1 () 및 getChoice2 () 내부의 storyNumber 속성을 사용하여 항상 첫 번째 (0 번째) 항목이 아닌 업데이트 된 스토리와 선택 항목을 가져옵니다.
 
-//TODO: Step 17 - Create a method called nextStory(), it should not have any outputs but it should have 1 input called choiceNumber which will be the choice number (int) made by the user.
+// TODO : 8 단계-_storyData에서 첫 번째 storyTitle을 반환하는 getStory ()라는 메서드를 만듭니다.
 
-//TODO: Step 20 - Download the story plan here: https://drive.google.com/uc?export=download&id=1KU6EghkO9Hf2hRM0756xFHgNaZyGCou3
+// TODO : 11 단계-_storyData에서 첫 번째 선택 1에 대한 텍스트를 반환하는 getChoice1 ()이라는 메서드를 만듭니다.
 
-//TODO: Step 21 - Using the story plan, update nextStory() to change the storyNumber depending on the choice made by the user. e.g. if choiceNumber was equal to 1 and the storyNumber is 0, the storyNumber should become 2.
+String getChoice1(){
 
-//TODO: Step 22 - In nextStory() if the storyNumber is equal to 3 or 4 or 5, that means it's the end of the game and it should call a method called restart() that resets the storyNumber to 0.
+  return _storyData[_storyNumber].choice1;
+}
 
-//TODO: Step 27 - Create a method called buttonShouldBeVisible() which checks to see if storyNumber is 0 or 1 or 2 (when both buttons should show choices) and return true if that is the case, else it should return false.
+// TODO : 12 단계-_storyData에서 첫 번째 선택 2에 대한 텍스트를 반환하는 getChoice2 ()라는 메서드를 만듭니다.
+
+  String getChoice2(){
+
+    return _storyData[_storyNumber].choice2;
+  }
+// TODO : 25 단계-story_brain.dart 만 액세스 할 수 있도록 storyNumber 속성을 개인 속성으로 변경합니다. 이름 (storyNumber)을 마우스 오른쪽 버튼으로 클릭하고 Refactor-> Rename을 선택하여 사용되는 모든 위치에서 변경할 수 있습니다.
+
+// TODO : 16 단계-값 0으로 시작하는 storyNumber라는 속성을 만듭니다. 이것은 사용자가 현재보고있는 스토리를 추적하는 데 사용됩니다.
+  int _storyNumber = 0;
+
+// TODO : 17 단계-nextStory ()라는 메서드를 생성합니다. 출력이 없어야하지만  사용자가 만든 선택 번호 (int)가 될 choiceNumber라는 입력이 1 개 있어야합니다. (모르겠음 ㅈㄴ 어렵다)
+  void nextStory(int choiceNumber){
+if(choiceNumber == 1 &&_storyNumber == 0){
+  _storyNumber = 2;
+}
+
+  }
+
+// TODO : 20 단계-여기에서 스토리 계획 다운로드 : https://drive.google.com/uc?export=download&id=1KU6EghkO9Hf2hRM0756xFHgNaZyGCou3
+
+// TODO : 21 단계-스토리 계획을 사용하여 nextStory ()를 업데이트하여 사용자의 선택에 따라 storyNumber를 변경합니다. 예 : choiceNumber가 1이고 storyNumber가 0이면 storyNumber는 2가되어야합니다.
+
+// TODO : Step 22-nextStory ()에서 storyNumber가 3, 4 또는 5와 같으면 게임이 끝났음을 의미하며 storyNumber를 0으로 재설정하는 restart ()라는 메서드를 호출해야합니다.
+
+// TODO : 27 단계-storyNumber가 0 또는 1 또는 2 (두 버튼 모두 선택 사항을 표시해야하는 경우)인지 확인하고이 경우 true를 반환하고 그렇지 않으면 false를 반환하는 buttonShouldBeVisible ()이라는 메서드를 만듭니다.
+}
